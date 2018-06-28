37ea39c4dcea72f51f3233c1f1300aee17e0ad46
loweiling <ubuntu@ip-172-31-92-101.ec2.internal>
2018-06-28 15:44:04 +0000

    modify: minuku.py

--- minuku.py
+++ minuku.py
@@ -4,7 +4,9 @@ from html.parser import HTMLParser
 from pymongo import MongoClient,InsertOne, DeleteOne, ReplaceOne
 from pymongo.cursor import CursorType
 from pymongo.collection import Collection
+from bson.json_util import dumps
 import urllib.request
+import time
 app = Flask(__name__)
 client = MongoClient('localhost', 27017)#make a connection, which means you can connet to any database within this host
 db = client.minukuTestDB# get a db
@@ -18,11 +20,11 @@ def login():
 	if request.method=='POST':
 		request_message = request.get_json()
 		for item in accountCollection.find():
-			if request_message['signupAccount'] == item['signupAccount']:
-				if request_message['signupPassword'] == item['signupPassword']:
+			if request_message['signupAccount'] == item['profile']['signupAccount']:
+				if request_message['signupPassword'] == item['profile']['signupPassword']:
 					#return json.jsonify(name="correct account",password="")
 					#return "correct account"
-					return make_response(json.jsonify({'msg':'success','userName':item['userName'],'signupAccount':item['signupAccount']}),200)
+					return make_response(json.jsonify({'msg':'success','userName':item['profile']['userName'],'signupAccount':item['profile']['signupAccount']}),200)
 				#else :return json.jsonify(name="",password="wrong password")
 				#else : return "wrong Password"
 				else : return make_response(json.jsonify({'error':'wrong password'}),404)		
@@ -33,18 +35,15 @@ def login():
 def signup():
 	if request.method=='POST':
 		request_message = request.get_json()    
-		account = {'signupAccount':request_message['signupAccount'],'userName':request_message['userName'],'signupPassword':request_message['signupPassword'],'signupEmail':request_message['signupEmail']}
+		account = {'signupAccount':request_message['signupAccount'],'userName':request_message['userName'],'signupPassword':request_message['signupPassword'],'signupEmail':request_message['signupEmail'],'signupTime':time.strftime("%c")}
 		for item in accountCollection.find():
-			if request_message['signupAccount'] == item['signupAccount']:
+			if request_message['signupAccount'] == item['profile']['signupAccount']:
 				return make_response(json.jsonify({'error':'this account already used'}),404)
-		accountCollection.insert_one(account)
+		accountCollection.insert_one({'profile':account})
 		return make_response(json.jsonify({'msg':'create account success','userName':request_message['userName'],'signupAccount':request_message['signupAccount']}),200)
 
-@app.route('/queryProfile',methods=['GET','POST'])
-def queryProfile():
+@app.route('/<string:signupAccount>/profile',methods=['GET'])
+def query_profile(signupAccount):
 	if request.method =='GET':
-		request_message = request.get_json()
-		for item in accountCollection.find():
-			if request_message['signupAccount']== item['signupAccount']:
-				return make_response(json.jsonify({'signupAccount':item['signupAccount'],'userName':item['userName'],'signupPassword':item['signupPassword'],'signupEmail':item['signupEmail']}),200)
-
+		data = accountCollection.find({'profile.signupAccount':signupAccount},{'profile':1})
+		return make_response(dumps(data[0]),200)

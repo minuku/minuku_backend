7211a0b61d01541acaeb37932582d76880ce6082
loweiling <ubuntu@ip-172-31-92-101.ec2.internal>
2018-07-05 07:55:13 +0000

    Modify : response message

--- api/user/views.py
+++ api/user/views.py
@@ -7,16 +7,18 @@ from bson.json_util import dumps
 def query_profile(account):
         if request.method =='GET':
                 data = db.accountCollection.find({'profile.account':account},{'profile':1})
-                return make_response(dumps(data[0]),200)
+                return make_response(dumps(data[0]['profile']),200)
 
 
 @user_blueprint.route('/profile2',methods=['GET'])
 def query_profile2():
-        if request.method == 'GET':
+	if request.method == 'GET':
                 #url = urlrequest.Request.get_full_url()
-                url = request.url
-                query_component = parse.urlparse(url).query
-                account = parse.parse_qs(query_component)['account'][0]
-                data = db.accountCollection.find({'profile.account':account},{'profile':1})
-                return make_response(dumps(data[0]),200)
+		url = request.url
+		query_component = parse.urlparse(url).query
+		account = parse.parse_qs(query_component)['account'][0]
+		
+		data = db.accountCollection.find({'profile.account':account})
+		#data = data[0]['profile']
+		return make_response(dumps(data[0]['profile']),200)
 
